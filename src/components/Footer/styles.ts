c714e40fee84d2e066d6c6116b1f06c414145cfa
Jerom Verschoote <33222896+jeromverschoote@users.noreply.github.com>
2021-11-24 20:33:13 +0100

    :lipstick: Adjust stlying

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,5 @@
 const styles = {
-  container: 'flex items-center justify-center w-full h-24 border-t',
+  container: 'flex items-center justify-center w-full h-24 border-t -mt-24',
   span: 'h-4 ml-2',
 };
 
