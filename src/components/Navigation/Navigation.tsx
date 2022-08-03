f41b25c8b6f8c74c826b3c3d35218951ca68697a
Jerom Verschoote <33222896+jeromverschoote@users.noreply.github.com>
2022-08-03 12:46:07 +0200

    :heavy_plus_sign: Add support Tailwind UI components.

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import { FC, useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import Context from 'context';
+
+import Button from 'components/Button';
+
+import { styles } from '.';
+
+const Navigation: FC = () => {
+  const { t } = useTranslation();
+
+  const { me, connect, disconnect } = useContext(Context.Wallet);
+
+  const isConnected = me !== null;
+
+  return (
+    <header className={styles.container}>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className={styles.span}>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={100} height={24} />
+        </span>
+      </a>
+
+      {isConnected ? (
+        <Button.Secondary onClick={() => disconnect()}>
+          {me?.address}
+        </Button.Secondary>
+      ) : (
+        <Button.Primary onClick={() => connect({ address: '0x0' })}>
+          {t('label.toConnect')}
+        </Button.Primary>
+      )}
+    </header>
+  );
+};
+
+export default Navigation;
